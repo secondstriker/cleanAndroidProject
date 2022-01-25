536fce32a6b448d5df0d0ce70fab960bcd8729f1
Mohsen Sedighi <sedighi.66@gmail.com>
2022-01-25 16:05:30 +0330

    use another api key due to limitations it has for just 50 fetch in every 12 hours.

--- app/src/main/java/com/codewithmohsen/lastnews/api/Constants.kt
+++ app/src/main/java/com/codewithmohsen/lastnews/api/Constants.kt
@@ -3,7 +3,7 @@ package com.codewithmohsen.lastnews.api
 object Constants {
 
     const val BASE_URL = "https://newsapi.org/v2/"
-    const val API_KEY = "2af6a4552d8c48a6a126cf6c876a3795"
+    const val API_KEY = "4803e6b0c58d42cead1a86a6727cb49b"
     const val REQUEST_TIMEOUT_IN_SECONDS = 10L
     const val READ_TIMEOUT_IN_SECONDS = 10L
     const val CONNECT_TIMEOUT_IN_SECONDS = 10L
